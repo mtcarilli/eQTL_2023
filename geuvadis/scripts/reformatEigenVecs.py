b23accc0110b5b3342ad8c810130485d8f4e2f92
mtcarilli <68964724+mtcarilli@users.noreply.github.com>
2023-06-02 13:17:42 -0700

    Create reformatEigenVecs.py

---
+++ geuvadis/scripts/reformatEigenVecs.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str)
+args = parser.parse_args()
+
+name = args.name
+
+eigenvecs = pd.read_table(f'../data/{name}.eigenvec',header=None,sep=' ')
+eigenvecs[1] = [ '0_' + eigenvecs[1][i] for i in range(len(eigenvecs))]
+eigenvecs = eigenvecs.T.drop(0)
+eigenvecs.to_csv(f'../data/{name}.eigenvec.T',header=None,sep = '\t',index=True)
+print('DONE with eigen reformatting')
