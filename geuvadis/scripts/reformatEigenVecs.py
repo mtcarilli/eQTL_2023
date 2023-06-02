e36030276d842e0513e887c7d918c146ac99c8fc
mtcarilli <68964724+mtcarilli@users.noreply.github.com>
2023-06-02 13:21:40 -0700

    Update reformatEigenVecs.py

--- geuvadis/scripts/reformatEigenVecs.py
+++ geuvadis/scripts/reformatEigenVecs.py
@@ -9,7 +9,7 @@ args = parser.parse_args()
 
 name = args.name
 
-eigenvecs = pd.read_table(f'../data/{name}.eigenvec',header=None,sep=' ')
+eigenvecs = pd.read_table(f'../genotypeData/{name}.eigenvec',header=None,sep=' ')
 eigenvecs[1] = [ '0_' + eigenvecs[1][i] for i in range(len(eigenvecs))]
 eigenvecs = eigenvecs.T.drop(0)
 eigenvecs.to_csv(f'../data/{name}.eigenvec.T',header=None,sep = '\t',index=True)
